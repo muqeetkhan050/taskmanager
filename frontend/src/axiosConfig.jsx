9de60968c839575d1eff0e9543597df0b6031220
muqeetkhan <muqeetkhan050@gmail.com>
2025-08-12 22:10:51 +1000

    restared pm2 due to failure of ec2

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://16.176.172.42:5001', // local
+  baseURL: 'http://16.176.228.138:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
